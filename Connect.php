537124eccca8c9f4a025405098c2d23fc73a5bed
gyocran <justblaze.0@gmail.com>
2016-02-25 13:30:24 +0000

    Added files via upload
    Skeleton code

---
+++ Connect.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        This is our skeleton code. Simon, Maame, Eric and George is the dream 
+        team!
+        <?php
+            
+        ?>
+    </body>
+</html>
