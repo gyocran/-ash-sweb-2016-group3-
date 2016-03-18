5460dd1543273967c7a231bf6934627539d3ec1f
unknown <justblaze.0@gmail.com>
2016-03-18 13:43:20 +0000

    created function to get bookings by date

--- booking.php
+++ booking.php
@@ -3,7 +3,9 @@
 include_once("adb.php");
 class booking extends adb
 {
-
+	function booking(){
+	}
+	
 	function getUserID()
 	{
 	$strQuery = "select user_id from sweb_booking left join sweb_user on sweb_booking.user_id = sweb_user.user_id"; 
@@ -62,13 +64,19 @@ class booking extends adb
 	
 	
 	function viewBooking()
-	{	
+	{
 	$userID = $this -> getUserID();
 	$strQuery ="select booking_id, labname, bookingdate, start_time, end_time, bookingstatus from sweb_booking where user_id = $userID";
 	
 	return $this -> query($strQuery);
 	}
 	
+	function viewBookingByDate($date)
+	{	
+	$strQuery ="select booking_id, labname, bookingdate, start_time, end_time, bookingstatus from sweb_booking where bookingdate = $date";
+	
+	return $this -> query($strQuery);
+	}
 }
 
 ?>
