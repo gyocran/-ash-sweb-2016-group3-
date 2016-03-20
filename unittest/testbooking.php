130b51b123257922613b2be2ed091970f6f70e06
simon <simon.suuk@outlook.com>
2016-03-20 08:29:09 +0000

    unit test added for updateBooking() method in booking class

--- unittest/testbooking.php
+++ unittest/testbooking.php
@@ -3,16 +3,29 @@ include_once("..\booking.php");
 
 class testbooking extends PHPUnit_Framework_TestCase{
 
-	public function testDeleteBooking(){
+	public function testUpdateBooking(){
 		//arrange
 		$bookingObj=new booking();
+		$eventName = random_bytes(10);
 	
-		//act
-		$this->assertEquals(true, $bookingObj->deleteBooking(4) );
-	
+		//act and assert
+		$this->assertEquals(true, 
+			$bookingObj->updateBooking(
+			4,			        //booking id
+			2, 			        // user id
+			"arts factory",	              //organization name
+			$eventName,		         //event name
+			"come let us teach you some salsa moves!",           //event description
+			"lab221",				//labname
+			"2016-04-02",			//booking date
+			"9:00-10:00 am"				//bookingime 
+			)
+		);
+		
 		//assert
-		$bookingObj->getBooking("booking_id = '4' ");
-		$this->assertEquals(null, $bookingObj->fetch());
+		$bookingObj->getBooking("event_name = '$eventName' ");
+		$this->assertEquals(1, $bookingObj->getNumRows());
+	
 	}
 	
 }
