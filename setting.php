765741d3dd9b1476514f60173273b8931e29daf1
ericgbekor <ericgbekor@gmail.com>
2016-04-22 01:56:38 +0000

    setting file added

--- setting.php
+++ setting.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST","localhost");
-define("DB_USERNAME","simon");
-define("DB_PASSWORD","phpdev");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
 define("DB_NAME","ash_sweb_group_3_db");
 ?>
