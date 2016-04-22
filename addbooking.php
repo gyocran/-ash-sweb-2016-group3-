2377d9dcb473a5ac700c33c4d6884b7418b81fff
Maame Afriyie Poku <Maame Afriyie Poku>
2016-04-22 04:05:17 +0000

    The booking form has been updated to include Java Script

--- addbooking.php
+++ addbooking.php
@@ -16,6 +16,64 @@
 		<title>Add New Booking</title>
 
 		<link rel="stylesheet" href="style.css"> 
+
+		<link rel="stylesheet" type="text/css" href="js/codebase/themes/message_default.css">
+		<link rel="stylesheet" type="text/css" href="js/codebase/dhtmlx.css"/>
+		<script type="text/javascript" src="js/jquery-1.12.1.js"></script>
+		<script type="text/javascript" src='js/codebase/message.js'></script>
+
+		<script type="text/javascript">
+
+			//This function makes requests for the ajax page to add a booking
+			function addbooking()
+			{	
+				//the various valaues entered by the user are stored in variables
+				var name = $("#textname").val();
+				var id = $("#id").val();
+				var cmd =$("#cmd").val();
+				var org_name = $("#org_name").val();
+				var event_name = $("#event_name").val();
+				var event_description = $("#event_description").val();
+				var labname =  $("#labname").val();
+				var bookingdate =  $("#bookingdate").val();
+				var bookingtime =  $("#bookingtime").val();
+
+				//The variables are added to the ajax page url
+				var ajaxurl = "bookingajax.php?"+'id ='+ id + '&cmd ='+ cmd +'&org_name ='+ org_name + '&event_name =' + event_name + '&event_description =' + event_description +'&labname =' + labname  + '&bookingdate =' + bookingdate + '&bookingtime =' + bookingtime;
+				
+				//This checks if all fields are filled
+				if (org_name == '' || event_name == '' || event_description == '' ||labname == '' || bookingdate == ''  || bookingtime == '') {
+				alert("Please Fill All Fields");
+				}
+
+				else{
+					//Makes a request to the ajax page with the following settings
+					$.ajax( ajaxurl, {
+							async:true,
+							complete: addbookingcomplete //calls the addbookingcomplete function after the request is complete
+						}
+					);		
+				}
+			}
+			
+			//This function recieves the result from the ajax page after the request has been made
+			function addbookingcomplete(xhr,status){
+				
+				if (status!="status"){
+					alert("Error while adding booking"); //checks if request was carried out successfully and prints out an alert if it was not
+					return;
+				}
+
+				var obj = $.parseJSON(xhr.resposeText); //converts the JSON object 
+
+				alert(obj.message);     //displays the JSON message
+
+	
+			}			
+		</script>
+	</head>
+
+
 	<body>
 		<header  id="pageheader"> 
 			<div style="width:10%; height:100%;float: left;"></div>
