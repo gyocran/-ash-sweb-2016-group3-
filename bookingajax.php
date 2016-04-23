01422f9ea255a631ad5b6b148abc8f41f77e5e42
simon <simon.suuk@outlook.com>
2016-04-23 20:19:25 +0000

    ajax page for delete booking modified

--- bookingajax.php
+++ bookingajax.php
@@ -9,11 +9,14 @@
 	$cmd=$_REQUEST['cmd'];
 	switch($cmd)
 	{
+		case 2:
+			myBookings();
+			break;
 		case 3:
 			deleteBooking();
 			break;
 		default:
-			echo "wrong cmd";	//change to json message
+			echo '{"result":0, "message":"wrong command"}';
 			break;
 	}
 	
@@ -45,5 +48,51 @@
 			echo '{"result":1, "message":"Booking Successfully Deleted"}';
 		}
 		
+	}
+	
+	// function to view user booking
+	function myBookings(){
+		include_once("booking.php");
+		//check if there is a user code
+		if(!isset($_REQUEST['id'])){
+			echo '{"result":0,"message":"User code not provided"}';		
+			return;
+		}
+		$userID=$_REQUEST["id"];
+		
+		//create an object of booking
+		$book=new booking();
+		
+		// call get user method
+		$view=$book->viewMyBooking($userID);
+
+		
+		// checking if bookings have been gotten from database
+		if($view==false){
+			echo '{"result":0,"message":"User code not provided"}';	
+			return;
+		}
+		
+		// getting the bookings 
+		$db = $book -> fetch();
+		
+		// checking for bookings and printing them in JSON format
+		if ($db==false)
+		{
+			echo "Error";
+		}
+		else{
+		echo '{"result":1,"booking":[';
+		   while($db!=false){
+			echo json_encode($db);
+			$db = $book -> fetch();
+			if ($db)
+			{
+				echo ',';	
+			}
+
+		   }
+		echo ']}';
+		}
 	}
 ?>
