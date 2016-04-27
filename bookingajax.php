24d7255802541d8896557c5e52bb29abe698c6b1
Maame Afriyie Poku <Maame Afriyie Poku>
2016-04-27 00:45:39 +0000

    Ajax PHP updated

--- bookingajax.php
+++ bookingajax.php
@@ -58,8 +58,49 @@
 			}
 			else{
 			
-				echo  '{"result":0, "message":"Booking was not added"}'; //JSON message sent
+				echo  '{"result":0, "message":"Booking was not added. This time slot has already been taken for this lab"}'; //JSON message sent
 			}			
+	}
+
+	function myBookings() {
+	    include_once("booking.php");
+	    //check if there is a user code
+	    if (!isset($_REQUEST['id'])) {
+	        echo '{"result":0,"message":"User code not provided"}';
+	        return;
+	    }
+	    $userID = $_REQUEST["id"];
+
+	    //create an object of booking
+	    $book = new booking();
+
+	    // call get user method
+	    $view = $book->viewMyBooking($userID);
+
+
+	    // checking if bookings have been gotten from database
+	    if ($view == false) {
+	        echo '{"result":0,"message":"User code not provided"}';
+	        return;
+	    }
+
+	    // getting the bookings 
+	    $db = $book->fetch();
+
+	    // checking for bookings and printing them in JSON format
+	    if ($db == false) {
+	        echo "Error";
+	    } else {
+	        echo '{"result":1,"booking":[';
+	        while ($db != false) {
+	            echo json_encode($db);
+	            $db = $book->fetch();
+	            if ($db) {
+	                echo ',';
+	            }
+	        }
+	        echo ']}';
+	    }
 	}
 
 ?>
